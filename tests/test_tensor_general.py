b29f751ec62deed182c8903beb5ac233e7a6adfb
malcolmyanguci <gufengy@uci.edu>
2020-11-07 19:31:13 -0800

    cuda-Q3.3 2

--- tests/test_tensor_general.py
+++ tests/test_tensor_general.py
@@ -4,7 +4,7 @@ from hypothesis import given
 import numba
 from hypothesis.strategies import floats, integers, lists, data, permutations
 from .strategies import tensors, shaped_tensors, assert_close
-from minitorch.tensor_data import shape_broadcast
+
 
 small_floats = floats(min_value=-100, max_value=100, allow_nan=False)
 v = 4.524423
@@ -67,114 +67,104 @@ def test_one_args(fn, backend, data):
         assert_close(t2[ind], fn[1](minitorch.Scalar(t1[ind])).data)
 
 
-# @given(data())
-# @pytest.mark.parametrize("fn", two_arg)
-# @pytest.mark.parametrize("backend", backend_tests)
-# def test_two_args(fn, backend, data):
-#     "Run forward for all two arg functions above."
-#     t1, t2 = data.draw(shaped_tensors(2, backend=backend))
-#     t3 = fn[1](t1, t2)
-#     for ind in t3._tensor.indices():
-#         assert (
-#             t3[ind] == fn[1](minitorch.Scalar(t1[ind]), minitorch.Scalar(t2[ind])).data
-#         )
+@given(data())
+@pytest.mark.parametrize("fn", two_arg)
+@pytest.mark.parametrize("backend", backend_tests)
+def test_two_args(fn, backend, data):
+    "Run forward for all two arg functions above."
+    t1, t2 = data.draw(shaped_tensors(2, backend=backend))
+    t3 = fn[1](t1, t2)
+    for ind in t3._tensor.indices():
+        assert (
+            t3[ind] == fn[1](minitorch.Scalar(t1[ind]), minitorch.Scalar(t2[ind])).data
+        )
 
 
-# @given(data())
-# @pytest.mark.parametrize("fn", one_arg)
-# @pytest.mark.parametrize("backend", backend_tests)
-# def test_one_derivative(fn, backend, data):
-#     "Run backward for all one arg functions above."
-#     t1 = data.draw(tensors(backend=backend))
-#     minitorch.grad_check(fn[1], t1)
+@given(data())
+@pytest.mark.parametrize("fn", one_arg)
+@pytest.mark.parametrize("backend", backend_tests)
+def test_one_derivative(fn, backend, data):
+    "Run backward for all one arg functions above."
+    t1 = data.draw(tensors(backend=backend))
+    minitorch.grad_check(fn[1], t1)
 
 
-# @given(data())
-# @pytest.mark.parametrize("fn", two_arg)
-# @pytest.mark.parametrize("backend", backend_tests)
-# def test_two_grad(fn, backend, data):
-#     "Run backward for all two arg functions above."
-#     t1, t2 = data.draw(shaped_tensors(2, backend=backend))
-#     minitorch.grad_check(fn[1], t1, t2)
+@given(data())
+@pytest.mark.parametrize("fn", two_arg)
+@pytest.mark.parametrize("backend", backend_tests)
+def test_two_grad(fn, backend, data):
+    "Run backward for all two arg functions above."
+    t1, t2 = data.draw(shaped_tensors(2, backend=backend))
+    minitorch.grad_check(fn[1], t1, t2)
 
 
-# @given(data())
-# @pytest.mark.parametrize("fn", reduce)
-# @pytest.mark.parametrize("backend", backend_tests)
-# def test_reduce(fn, backend, data):
-#     "Run backward for all reduce functions above."
-#     t1 = data.draw(tensors(backend=backend))
-#     minitorch.grad_check(fn[1], t1)
+@given(data())
+@pytest.mark.parametrize("fn", reduce)
+@pytest.mark.parametrize("backend", backend_tests)
+def test_reduce(fn, backend, data):
+    "Run backward for all reduce functions above."
+    t1 = data.draw(tensors(backend=backend))
+    minitorch.grad_check(fn[1], t1)
 
 
-# @given(data())
-# @pytest.mark.parametrize("fn", two_arg)
-# @pytest.mark.parametrize("backend", backend_tests)
-# def test_two_grad_broadcast(fn, backend, data):
-#     "Run backward for all two arg functions above with broadcast."
+@given(data())
+@pytest.mark.parametrize("fn", two_arg)
+@pytest.mark.parametrize("backend", backend_tests)
+def test_two_grad_broadcast(fn, backend, data):
+    "Run backward for all two arg functions above with broadcast."
 
-#     t1, t2 = data.draw(shaped_tensors(2, backend=backend))
-#     minitorch.grad_check(fn[1], t1, t2)
+    t1, t2 = data.draw(shaped_tensors(2, backend=backend))
+    minitorch.grad_check(fn[1], t1, t2)
 
-#     # broadcast check
-#     minitorch.grad_check(fn[1], t1.sum(0), t2)
-#     minitorch.grad_check(fn[1], t1, t2.sum(0))
-
-
-# @given(data())
-# @pytest.mark.parametrize("backend", backend_tests)
-# def test_permute(backend, data):
-#     "Check permutations for all backends."
-#     t1 = data.draw(tensors(backend=backend))
-#     permutation = data.draw(permutations(range(len(t1.shape))))
-
-#     def permute(a):
-#         return a.permute(*permutation)
+    # broadcast check
+    minitorch.grad_check(fn[1], t1.sum(0), t2)
+    minitorch.grad_check(fn[1], t1, t2.sum(0))
 
 
-#     minitorch.grad_check(permute, t1)
+@given(data())
+@pytest.mark.parametrize("backend", backend_tests)
+def test_permute(backend, data):
+    "Check permutations for all backends."
+    t1 = data.draw(tensors(backend=backend))
+    permutation = data.draw(permutations(range(len(t1.shape))))
+
+    def permute(a):
+        return a.permute(*permutation)
+
+    minitorch.grad_check(permute, t1)
 
 
-# @pytest.mark.task3_2
-# def test_mm2():
-#     a = minitorch.rand((2, 3), backend=FastTensorBackend)
-#     b = minitorch.rand((3, 4), backend=FastTensorBackend)
-#     c = a @ b
+@pytest.mark.task3_2
+def test_mm2():
+    a = minitorch.rand((2, 3), backend=FastTensorBackend)
+    b = minitorch.rand((3, 4), backend=FastTensorBackend)
+    c = a @ b
 
-#     c2 = (a.view(2, 3, 1) * b.view(1, 3, 4)).sum(1).view(2, 4)
-#     # print("a",a.shape)
-#     # print("b",b.shape)
-#     # print("boardcasting shape: ",shape_broadcast(a.shape[:-2], b.shape[:-2]))
-#     # print("c2",c2.shape)
-#     # assert_close(0,1)
+    c2 = (a.view(2, 3, 1) * b.view(1, 3, 4)).sum(1).view(2, 4)
 
-#     for ind in c._tensor.indices():
-#         assert_close(c[ind], c2[ind])
+    for ind in c._tensor.indices():
+        assert_close(c[ind], c2[ind])
 
-# # Matrix Multiplication
-# @given(data())
-# @pytest.mark.parametrize("backend", matmul_tests)
-# def test_mm(backend, data):
-#     small_ints = integers(min_value=2, max_value=4)
-#     A, B, C, D = (
-#         data.draw(small_ints),
-#         data.draw(small_ints),
-#         data.draw(small_ints),
-#         data.draw(small_ints),
-#     )
-#     a = data.draw(tensors(backend=backend, shape=(D, A, B)))
-#     b = data.draw(tensors(backend=backend, shape=(1, B, C)))
 
-#     c = a @ b
-#     c2 = (
-#         (a.contiguous().view(D, A, B, 1) * b.contiguous().view(1, 1, B, C))
-#         .sum(2)
-#         .view(D, A, C)
-#     )
-#     print("a",a.shape)
-#     print("b",b.shape)
-#     print("c2",c2)
-#     print("c",c)
+# Matrix Multiplication
+@given(data())
+@pytest.mark.parametrize("backend", matmul_tests)
+def test_mm(backend, data):
+    small_ints = integers(min_value=2, max_value=4)
+    A, B, C, D = (
+        data.draw(small_ints),
+        data.draw(small_ints),
+        data.draw(small_ints),
+        data.draw(small_ints),
+    )
+    a = data.draw(tensors(backend=backend, shape=(D, A, B)))
+    b = data.draw(tensors(backend=backend, shape=(1, B, C)))
 
-#     for ind in c._tensor.indices():
-#         assert_close(c[ind], c2[ind])
+    c = a @ b
+    c2 = (
+        (a.contiguous().view(D, A, B, 1) * b.contiguous().view(1, 1, B, C))
+        .sum(2)
+        .view(D, A, C)
+    )
+    for ind in c._tensor.indices():
+        assert_close(c[ind], c2[ind])
